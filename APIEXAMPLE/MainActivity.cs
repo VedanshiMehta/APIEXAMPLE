9fb165fea759bdd3ac100720c3d11371aded3784
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-04-19 17:37:44 +0530

    Add project files.

---
+++ APIEXAMPLE/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using APIEXAMPLE.Model;
+using Newtonsoft.Json;
+using Org.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace APIEXAMPLE
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private TextView _textView;
+        private List<WeatherDetails> wd= new List<WeatherDetails>();
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            _textView = FindViewById<TextView>(Resource.Id.textView1);
+            GetDataAsync();
+        }
+
+        private async Task GetDataAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1");
+            if (response.IsSuccessStatusCode)
+            {
+               
+                var jasonStringWeather = await response.Content.ReadAsStringAsync();
+
+                //JSONObject jSONObject = new JSONObject(jasonStringWeather);
+              
+               
+                
+               
+              var weatherDetails = JsonConvert.DeserializeObject<WeatherDetails>(jasonStringWeather);
+
+         
+               
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ APIEXAMPLE/Model/WeatherDetails.cs
@@ -0,0 +1,154 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace APIEXAMPLE.Model
+{
+    class WeatherDetails
+    {
+        [JsonProperty("cod")]
+        public string Cod { get; set; }
+
+        [JsonProperty("message")]
+        public double Message { get; set; }
+
+        [JsonProperty("cnt")]
+        public int Cnt { get; set; }
+
+        [JsonProperty("list")]
+        public List<List> List { get; set; }
+
+        [JsonProperty("city")]
+        public City City { get; set; }
+    }
+
+    public class Main
+    {
+        [JsonProperty("temp")]
+        public double Temp { get; set; }
+
+        [JsonProperty("temp_min")]
+        public double TempMin { get; set; }
+
+        [JsonProperty("temp_max")]
+        public double TempMax { get; set; }
+
+        [JsonProperty("pressure")]
+        public double Pressure { get; set; }
+
+        [JsonProperty("sea_level")]
+        public double SeaLevel { get; set; }
+
+        [JsonProperty("grnd_level")]
+        public double GrndLevel { get; set; }
+
+        [JsonProperty("humidity")]
+        public int Humidity { get; set; }
+
+        [JsonProperty("temp_kf")]
+        public double TempKf { get; set; }
+    }
+
+    public class Weather
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("main")]
+        public string Main { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+    }
+
+    public class Clouds
+    {
+        [JsonProperty("all")]
+        public int All { get; set; }
+    }
+
+    public class Wind
+    {
+        [JsonProperty("speed")]
+        public double Speed { get; set; }
+
+        [JsonProperty("deg")]
+        public double Deg { get; set; }
+    }
+
+    public class Snow
+    {
+        [JsonProperty("3h")]
+        public double? _3h { get; set; }
+    }
+
+    public class Sys
+    {
+        [JsonProperty("pod")]
+        public string Pod { get; set; }
+    }
+
+    public class List
+    {
+        [JsonProperty("dt")]
+        public int Dt { get; set; }
+
+        [JsonProperty("main")]
+        public Main Main { get; set; }
+
+        [JsonProperty("weather")]
+        public List<Weather> Weather { get; set; }
+
+        [JsonProperty("clouds")]
+        public Clouds Clouds { get; set; }
+
+        [JsonProperty("wind")]
+        public Wind Wind { get; set; }
+
+        [JsonProperty("snow")]
+        public Snow Snow { get; set; }
+
+        [JsonProperty("sys")]
+        public Sys Sys { get; set; }
+
+        [JsonProperty("dt_txt")]
+        public string DtTxt { get; set; }
+    }
+
+    public class Coord
+    {
+        [JsonProperty("lat")]
+        public double Lat { get; set; }
+
+        [JsonProperty("lon")]
+        public double Lon { get; set; }
+    }
+
+    public class City
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("coord")]
+        public Coord Coord { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+    }
+
+
+}
